49950618c214e63837bae8a0e7cf7dd1280f8e2e
brunoseleghin <brunoseleghin@gmail.com>
2024-10-07 19:19:36 -0300

    fix

---
+++ packages/docs/.storybook/preview.ts
@@ -0,0 +1,23 @@
+import type { Preview } from "@storybook/react";
+import { themes } from "storybook/internal/theming";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    docs: {
+      theme: themes.dark
+
+    },
+    backgrounds: {
+      default: 'dark'
+    }
+  },
+  tags: ['autodocs']
+};
+
+export default preview;
---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,28 @@
+import type { StoryObj, Meta } from "@storybook/react"
+
+import { Text, TextProps } from "@ignite-ui/react"
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+
+  args: {
+    children: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos eius quos ad veniam veritatis culpa quas totam, laboriosam beatae vero doloremque voluptatibus? Soluta eum minus reiciendis quo fuga, facilis distinctio.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: ['xxs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl', '4xl', '5xl', '6xl', '7xl', '8xl', '9xl'],
+      control: 'inline-radio',
+    },
+  }
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong'
+  }
+}
